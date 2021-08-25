89c453d81515be7dfef4e9f812adc3a66a72c300
Matt Joiner <anacrolix@gmail.com>
2021-08-25 15:19:54 +1000

    Unembed Cache from Blob

--- direct.go
+++ direct.go
@@ -149,7 +149,7 @@ func rowidForBlob(c conn, name string, length int64, create bool) (rowid int64, 
 type Blob struct {
 	Name   string
 	Length int64
-	*Cache
+	Cache  *Cache
 }
 
 func (p Blob) doAtIoWithBlob(
@@ -158,9 +158,9 @@ func (p Blob) doAtIoWithBlob(
 	off int64,
 	create bool,
 ) (n int, err error) {
-	p.l.Lock()
-	defer p.l.Unlock()
-	if p.opts.NoCacheBlobs {
+	p.Cache.l.Lock()
+	defer p.Cache.l.Unlock()
+	if p.Cache.opts.NoCacheBlobs {
 		defer p.forgetBlob()
 	}
 	blob, err := p.getBlob(create)
@@ -179,7 +179,7 @@ func (p Blob) doAtIoWithBlob(
 	// "ABORT" occurs if the row the blob is on is modified elsewhere. "ERROR: invalid blob" occurs
 	// if the blob has been closed. We don't forget blobs that are closed by our GC finalizers,
 	// because they may be attached to names that have since moved on to another blob.
-	if se.Code != sqlite.SQLITE_ABORT && !(p.opts.GcBlobs && se.Code == sqlite.SQLITE_ERROR && se.Msg == "invalid blob") {
+	if se.Code != sqlite.SQLITE_ABORT && !(p.Cache.opts.GcBlobs && se.Code == sqlite.SQLITE_ERROR && se.Msg == "invalid blob") {
 		return
 	}
 	p.forgetBlob()
@@ -206,53 +206,53 @@ func (p Blob) WriteAt(b []byte, off int64) (n int, err error) {
 }
 
 func (p Blob) SetTag(name string, value interface{}) error {
-	p.l.Lock()
-	defer p.l.Unlock()
-	return sqlitex.Exec(p.conn, "insert or replace into tag (blob_name, tag_name, value) values (?, ?, ?)", nil,
+	p.Cache.l.Lock()
+	defer p.Cache.l.Unlock()
+	return sqlitex.Exec(p.Cache.conn, "insert or replace into tag (blob_name, tag_name, value) values (?, ?, ?)", nil,
 		p.Name, name, value)
 }
 
 func (p Blob) forgetBlob() {
-	blob, ok := p.blobs[p.Name]
+	blob, ok := p.Cache.blobs[p.Name]
 	if !ok {
 		return
 	}
 	blob.Close()
-	delete(p.blobs, p.Name)
+	delete(p.Cache.blobs, p.Name)
 }
 
 func (p Blob) GetTag(name string, result func(*sqlite.Stmt)) error {
-	p.l.Lock()
-	defer p.l.Unlock()
-	return sqlitex.Exec(p.conn, "select value from tag where blob_name=? and tag_name=?", func(stmt *sqlite.Stmt) error {
+	p.Cache.l.Lock()
+	defer p.Cache.l.Unlock()
+	return sqlitex.Exec(p.Cache.conn, "select value from tag where blob_name=? and tag_name=?", func(stmt *sqlite.Stmt) error {
 		result(stmt)
 		return nil
 	}, p.Name, name)
 }
 
 func (p Blob) getBlob(create bool) (*sqlite.Blob, error) {
-	blob, ok := p.blobs[p.Name]
+	blob, ok := p.Cache.blobs[p.Name]
 	if !ok {
-		rowid, err := rowidForBlob(p.conn, p.Name, p.Length, create)
+		rowid, err := rowidForBlob(p.Cache.conn, p.Name, p.Length, create)
 		if err != nil {
 			return nil, fmt.Errorf("getting rowid for blob: %w", err)
 		}
-		blob, err = p.conn.OpenBlob("main", "blob", "data", rowid, true)
+		blob, err = p.Cache.conn.OpenBlob("main", "blob", "data", rowid, true)
 		if err != nil {
 			panic(err)
 		}
-		if p.opts.GcBlobs {
+		if p.Cache.opts.GcBlobs {
 			herp := new(byte)
 			runtime.SetFinalizer(herp, func(*byte) {
-				p.l.Lock()
-				defer p.l.Unlock()
+				p.Cache.l.Lock()
+				defer p.Cache.l.Unlock()
 				// Note there's no guarantee that the finalizer fired while this blob is the same
 				// one in the blob cache. It might be possible to rework this so that we check, or
 				// strip finalizers as appropriate.
 				blob.Close()
 			})
 		}
-		p.blobs[p.Name] = blob
+		p.Cache.blobs[p.Name] = blob
 	}
 	return blob, nil
 }
