db3546a4a883fa82ef6cafe7873d2f661d6ce5db
Matt Joiner <anacrolix@gmail.com>
2023-09-08 23:30:15 +1000

    Don't use the ridiculous Reader/Writer crawshaw.Blob interface

--- compat.go
+++ compat.go
@@ -1,43 +1,14 @@
 package squirrel
 
 import (
-	"fmt"
-	"io"
-
 	"github.com/go-llsqlite/adapter"
 )
 
 // Blob API differs between crawshaw and zombiezen.
 
 func blobReadAt(blob *sqlite.Blob, b []byte, off int64) (n int, err error) {
-	origOff, err := blob.Seek(0, io.SeekCurrent)
-	if err != nil {
-		err = fmt.Errorf("getting current offset: %w", err)
-		return
-	}
-	_, err = blob.Seek(off, io.SeekStart)
-	if err != nil {
-		return
-	}
-	n, err = blob.Read(b)
-	if err != nil {
-		return
-	}
-	finalOff, err := blob.Seek(origOff, io.SeekStart)
-	if err != nil {
-		err = fmt.Errorf("seeking to original offset: %w", err)
-		return
-	}
-	if finalOff != origOff {
-		panic("original offset not restored and no error given")
-	}
-	return
+	return blob.ReadAt(b, off)
 }
 func blobWriteAt(blob *sqlite.Blob, b []byte, off int64) (n int, err error) {
-	_, err = blob.Seek(off, io.SeekStart)
-	if err != nil {
-		return
-	}
-	n, err = blob.Write(b)
-	return
+	return blob.WriteAt(b, off)
 }
