49a2dbc677330a5d5811e11e01b7ab1ba3e05df7
Matt Joiner <anacrolix@gmail.com>
2023-09-09 00:06:34 +1000

    Add PinnedBlob.Reopen

--- pinned-blob.go
+++ pinned-blob.go
@@ -1,11 +1,29 @@
 package squirrel
 
-import "github.com/go-llsqlite/adapter"
+import (
+	"errors"
+	"github.com/go-llsqlite/adapter"
+)
 
 // Wraps a specific sqlite.Blob instance, when we don't want to dive into the cache to refetch blobs.
 type PinnedBlob struct {
 	blob *sqlite.Blob
 	c    *Cache
+}
+
+func (pb PinnedBlob) Reopen(name string) error {
+	pb.c.l.Lock()
+	defer pb.c.l.Unlock()
+	rowid, _, ok, err := rowidForBlob(pb.c.conn, name)
+	// If we fail between here and the reopen, the blob handle remains on the existing row.
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return errors.New("rowid for name not found")
+	}
+	// If this fails, the blob handle is aborted.
+	return pb.blob.Reopen(rowid)
 }
 
 // This is very cheap for this type.
--- raw.go
+++ raw.go
@@ -49,18 +49,23 @@ func createBlob(c conn, name string, length int64, clobber bool) (rowid int64, e
 }
 
 func rowidForBlob(c conn, name string) (rowid int64, length int64, ok bool, err error) {
-	// Holy shit. Use octet_length here for sqlite 3.43.0. Looks like this was loading the entire
-	// row to determine length.
-	err = sqlitex.Exec(c, "select data_id, length(data) from blob join blob_data using (data_id) where name=?", func(stmt *sqlite.Stmt) error {
-		if ok {
-			panic("expected at most one row")
-		}
-		// TODO: How do we know if we got this wrong?
-		rowid = stmt.ColumnInt64(0)
-		length = stmt.ColumnInt64(1)
-		ok = true
-		return nil
-	}, name)
+	err = sqlitex.Exec(
+		c,
+		// Holy shit. Use octet_length here for sqlite 3.43.0. Looks like this was loading the
+		// entire row to determine length.
+		"select data_id, length(data) from blob join blob_data using (data_id) where name=?",
+		func(stmt *sqlite.Stmt) error {
+			if ok {
+				panic("expected at most one row")
+			}
+			// TODO: How do we know if we got this wrong?
+			rowid = stmt.ColumnInt64(0)
+			length = stmt.ColumnInt64(1)
+			ok = true
+			return nil
+		},
+		name,
+	)
 	if err != nil {
 		return
 	}
